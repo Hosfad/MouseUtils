d4aa5c4192e5c815d346effa11539ffe485e18ee
Firas <Firassabea1@outlook.com>
2022-11-17 15:36:53 +0100

    Edit readme

--- PlotMouseData.py
+++ PlotMouseData.py
@@ -14,44 +14,7 @@ from sklearn.metrics import r2_score
 
 
 def circular_hist(ax, x, w, bins=16, density=False, offset=0, gaps=True):
-    """
-    Produce a circular histogram of angles on ax.
 
-    Parameters
-    ----------
-    ax : matplotlib.axes._subplots.PolarAxesSubplot
-        axis instance created with subplot_kw=dict(projection='polar').
-
-    x : array
-        Angles to plot, expected in units of radians.
-
-    bins : int, optional
-        Defines the number of equal-width bins in the range. The default is 16.
-
-    density : bool, optional
-        If True plot frequency proportional to area. If False plot frequency
-        proportional to radius. The default is True.
-
-    offset : float, optional
-        Sets the offset for the location of the 0 direction in units of
-        radians. The default is 0.
-
-    gaps : bool, optional
-        Whether to allow gaps between bins. When gaps = False the bins are
-        forced to partition the entire [-pi, pi] range. The default is True.
-
-    Returns
-    -------
-    n : array or list of arrays
-        The number of values in each bin.
-
-    bins : array
-        The edges of the bins.
-
-    patches : `.BarContainer` or list of a single `.Polygon`
-        Container of individual artists used to create the histogram
-        or list of such containers if there are multiple input datasets.
-    """
     # Wrap angles to [-pi, pi)
     x = (x + np.pi) % (2 * np.pi) - np.pi
 
--- README.md
+++ README.md
@@ -4,5 +4,8 @@ Human mouse utils (Collect your own mouse data and visualize it)
 Requirements : 
 Pynput, Matplotlib, Numpy
 
+CollectData.py collects yoo shit.
+PlotMoustData.py plots it
+
 Output: 
 ![Figure_1](https://user-images.githubusercontent.com/85018366/194695756-bf4fc3dc-e697-4478-babe-1adfae21e7a8.png)
